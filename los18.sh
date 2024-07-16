d184d844e8955abe17680145a4af822dd09cc979
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-07-16 12:43:09 -0300

    Update los18.sh

--- los18.sh
+++ los18.sh
@@ -3,14 +3,14 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
+repo init --depth=1 -u https://github.com/Project-Kizashi/android.gi -b relay --git-lfs
 
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/katakishi0/local_manifests --depth 1 -b 11.0 .repo/local_manifests
+git clone https://github.com/katakishi0/local_manifests --depth 1 -b kizashi .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -31,8 +31,8 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_channel-ap2-userdebug
+lunch lunch kasumi_channel-userdebug
 echo "============="
 
 # Build rom
-mka bacon
+mka bandori
